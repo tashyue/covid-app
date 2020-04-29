ba552b4596082f9fa64db4797a6b42d4c41e3b63
Kevin Prakash <kevprakash@gmail.com>
2020-04-29 02:59:42 -0700

    Moved States enum to its own file

--- src/main/java/covidapp/backend/CovidStateDataCollector.java
+++ src/main/java/covidapp/backend/CovidStateDataCollector.java
@@ -18,21 +18,6 @@ import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
  * @since 1.0
  */
 public class CovidStateDataCollector {
-
-	/**
-	 * 
-	 * Initials for all 50 states + DC
-	 * @TODO: Add more information to each state (full name, region, etc)
-	 *
-	 */
-	public enum State{
-		AL, AK, AZ, AR, CA, CO, CT, DE, DC, FL,
-		GA, HI, ID, IL, IN, IA, KS, KY, LA, ME,
-		MD, MA, MI, MN, MS, MO, MT, NE, NV, NH,
-		NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI,
-		SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY
-	}
-	
 	
 	/**
 	 * 
---
+++ src/main/java/covidapp/backend/State.java
@@ -0,0 +1,15 @@
+package covidapp.backend;
+
+/**
+ * 
+ * Initials for all 50 states + DC
+ * @TODO: Add more information to each state (full name, region, etc)
+ *
+ */
+public enum State{
+	AL, AK, AZ, AR, CA, CO, CT, DE, DC, FL,
+	GA, HI, ID, IL, IN, IA, KS, KY, LA, ME,
+	MD, MA, MI, MN, MS, MO, MT, NE, NV, NH,
+	NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI,
+	SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY
+}
