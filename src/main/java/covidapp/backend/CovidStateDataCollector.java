f243f4f393152cd5b828e3a55bbfc73372fc7db4
Kevin Prakash <kevprakash@gmail.com>
2020-04-29 02:26:17 -0700

    Added class for state-level data retrieval

---
+++ src/main/java/covidapp/backend/CovidStateDataCollector.java
@@ -0,0 +1,15 @@
+package covidapp.backend;
+
+/**
+ * 
+ * @author Kevin Prakash
+ *
+ * Meant for calls to collecting data at a state-level
+ * 
+ * @version 1.0
+ * @since 1.0
+ */
+public class CovidStateDataCollector {
+
+	
+}
