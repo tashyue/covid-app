791dbf6b1c3e387e1c348fc75241c3a03b6d5dc8
Kevin Prakash <kevprakash@gmail.com>
2020-04-30 15:04:27 -0700

    Moved logic from testing code to HelloController for consistency purposes

--- src/main/java/tashyue/covidapp/HelloController.java
+++ src/main/java/tashyue/covidapp/HelloController.java
@@ -10,23 +10,41 @@ import java.util.Set;
 
 import com.google.gson.Gson;
 
+import covidapp.backend.DynamoDBMediator;
 import covidapp.testing.DynamoDBMediatorTester;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 @RestController
 public class HelloController {
     @GetMapping("/api")
     public ArrayList<ArrayList<String[]>> hello() {
-    	
-    	ArrayList<ArrayList<String[]>> testData = DynamoDBMediatorTester.getItemTest();
         
         // Gson gson = new Gson();
         // ArrayList<String[]> json = gson.toJson(testData);
 
+    	DynamoDbClient client = DynamoDBMediator.getClient(Region.US_EAST_1);
+		ArrayList<String[]> stateData = new ArrayList<String[]>();
+		ArrayList<ArrayList<String[]>> allStates = new ArrayList<ArrayList<String[]>>();
+		covidapp.backend.State states[] = covidapp.backend.State.values();
+		System.out.println(states[0].getName());
+		for(int i = 0; i < covidapp.backend.State.values().length; i++) {
+			Map<String, AttributeValue> res = DynamoDBMediator.getDynamoDBItem(client, "covid-data", "state", states[i].toString());
+			res = DynamoDBMediator.filterResponse(res, "death", "hospitalizedCurrently", "dataQualityGrade");
+			stateData = new ArrayList<String[]>();
+			stateData.add(new String[]{states[i].name});
+			for (String k : res.keySet()) {
+				// Add key-value pair
+				String numericalValue = res.get(k).n();
+				stateData.add(new String[] {k, numericalValue != null ? numericalValue : res.get(k).s() });
+			}
+			allStates.add(stateData);
+		}
        
 
         
-        return testData;
+        return allStates;
         
     }
 }
