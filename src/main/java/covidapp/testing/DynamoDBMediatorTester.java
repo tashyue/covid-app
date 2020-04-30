1ab52dcac466b8a6a24944e3d19f5ea7a830ce0b
Kevin Prakash <kevprakash@gmail.com>
2020-04-29 19:15:49 -0700

    Create tester class for DynamoDBMediator

---
+++ src/main/java/covidapp/testing/DynamoDBMediatorTester.java
@@ -0,0 +1,27 @@
+package covidapp.testing;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import covidapp.backend.DynamoDBSMediator;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+public class DynamoDBMediatorTester {
+
+	public static ArrayList<String[]> getItemTest() {
+		DynamoDbClient client = DynamoDBSMediator.getClient(Region.US_EAST_1);
+		Map<String, AttributeValue> res = DynamoDBSMediator.getDynamoDBItem(client, "covid-data", "state", "CA");
+		
+		ArrayList<String[]> output = new ArrayList<String[]>();
+		for(String k : res.keySet()) {
+			//Add key-value pair
+			output.add(new String[] {
+					k,
+					res.get(k).toString()
+			});
+		}
+		return output;
+	}
+}
