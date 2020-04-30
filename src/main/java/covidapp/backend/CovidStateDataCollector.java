6f029c87cf0cafc5eff7cefd75c897c542d7ff6d
Kevin Prakash <kevprakash@gmail.com>
2020-04-29 19:01:20 -0700

    Updated client region call to match where the database is stored

--- src/main/java/covidapp/backend/CovidStateDataCollector.java
+++ src/main/java/covidapp/backend/CovidStateDataCollector.java
@@ -26,8 +26,8 @@ public class CovidStateDataCollector {
 	 * 
 	 */
 	public static List<Integer> getCovidDataForState(State s) {
-		//Assume Northern California client for now
-		DynamoDbClient client = DynamoDBSMediator.getClient(Region.US_WEST_1);
+		//Assume Northern Virginia region for now
+		DynamoDbClient client = DynamoDBSMediator.getClient(Region.US_EAST_1);
 		
 		return reformatData(DynamoDBSMediator.getDynamoDBItem(client, "covid-data", "State", s.name()));
 	}
